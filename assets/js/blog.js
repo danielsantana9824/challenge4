1b652d1cbe5df1cbfe989365259c7ce34edfea6d
danielsantana9824 <danielsantana9824@gmail.com>
2024-09-02 16:58:46 -0500

    9/2/2024 blog.html
    i finished this page, i needed to puto some lines, and finished the dark theme

--- assets/js/blog.js
+++ assets/js/blog.js
@@ -2,41 +2,27 @@ const section = document.querySelector("section");
 const bottomEl = document.getElementById("back");
 const saveButton = document.getElementById('save');
 
-const array = JSON.parse (localStorage.getItem("users"))|| [] ;
+const array = JSON.parse(localStorage.getItem("users")) || [];
 
 
 function loadBlogs() {
   section.textContent = "";
 
-  // const register = {
-  //   nameUser: "a",
-  //   title: "b",
-  //   content: "c"
-  // };
+  for (let i = 0; i < array.length; i++) {
+    section.innerHTML += `<article>
+    <h3>${array[i].title}</h3>
+    <hr>
+    <p class="sentence">${array[i].content}</p>
 
-  // array[1] = register;
+    <p>Posted By: <span>${array[i].nameUser}</span></p>
+    </article>`
 
-  console.log(array);
-  
-
-for (let i = 0; i < array.length; i++) {
-
-  console.log("a");
-  
-  section.innerHTML+=`<article>
-  <h3>${array[i].title}</h3>
-  <hr>
-  <p class="sentence">${array[i].content}</p>
-
-  <p>Posted By: <span>${array[i].nameUser}</span></p>
-</article>`
-  
-}
+  }
 
 }
 
 function goBack() {
-  window.history.back()
+  document.location.replace("index.html");
 }
 
 loadBlogs();
